72675c92c8d16e10efa4970ba95ace7f98fe2933
juanadls <eleo.dev09@gmail.com>
2024-12-19 11:09:57 -0300

    product card ui finished

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -17,6 +17,16 @@ class ProductCard extends StatelessWidget {
           children: [
             _BackgoundImage(),
             _ProductDetails(),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: _PriceTag(),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: _NotAvailableItem(),
+            )
           ],
         ),
       ),
@@ -32,17 +42,100 @@ class ProductCard extends StatelessWidget {
       );
 }
 
+class _NotAvailableItem extends StatelessWidget {
+  const _NotAvailableItem();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+          color: Colors.yellow[800],
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(25),
+            bottomRight: Radius.circular(25),
+          )),
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            "No disponible",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  const _PriceTag();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: 100,
+      height: 70,
+      decoration: const BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25), bottomLeft: Radius.circular(25))),
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            "103",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _ProductDetails extends StatelessWidget {
   const _ProductDetails();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
-      color: Colors.red,
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        decoration: _buildBoxDecoration(),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Disco durgo g",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              "Id del disco duro",
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  BoxDecoration _buildBoxDecoration() => const BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(25), topRight: Radius.circular(25)),
+      );
 }
 
 class _BackgoundImage extends StatelessWidget {
