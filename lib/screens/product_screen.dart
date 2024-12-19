6f934e4feaa5fe8af8e0ff320c8e6bef4d44322a
juanadls <eleo.dev09@gmail.com>
2024-12-19 12:00:33 -0300

    product screen modified

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_sample_app/ui/input_decorations.dart';
 import 'package:products_sample_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -6,18 +7,99 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             Stack(
               children: [
-                ProductImage(),
+                const ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
               ],
-            )
+            ),
+            const _ProductForm(),
+            const SizedBox(height: 100)
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save_outlined),
+      ),
     );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: _buildBoxDecoration(),
+        width: double.infinity,
+        child: Form(
+            child: Column(
+          children: [
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecorations.authInpuDecoration(
+                  hintText: "Nombre del producto", labelText: "Nombre: "),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecorations.authInpuDecoration(
+                  hintText: "150", labelText: "Precio: "),
+            ),
+            const SizedBox(height: 30),
+            SwitchListTile.adaptive(
+              value: true,
+              title: const Text("Disponible"),
+              activeColor: Colors.indigo,
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 30),
+          ],
+        )),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => const BoxDecoration(
+      color: Colors.white,
+      boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(0, 5))],
+      borderRadius: BorderRadius.only(
+        bottomRight: Radius.circular(25),
+        bottomLeft: Radius.circular(25),
+      ));
 }
